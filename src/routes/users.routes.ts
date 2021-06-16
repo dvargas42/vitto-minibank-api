b1993e2bc5cc1ac1f391656b395978da457787c3
dvargas42 <dvargas.eng@gmail.com>
2021-06-16 19:25:38 -0300

    feat: create user get router

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 import CreateUserService from '../services/CreateUserService';
+import ListUserDataService from '../services/ListUserDataService';
 
 const usersRouter = Router();
+
+usersRouter.get('/', ensureAuthenticated, async (request, response) => {
+  const { id } = request.user;
+
+  const listUser = new ListUserDataService()
+
+  const user = await listUser.execute({ id })
+
+  return response.json(user);
+});
 
 usersRouter.post('/', async (request, response) => {
     const { name, cpf, password } = request.body;
@@ -15,13 +28,8 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    const userWithoutPassword = {
-      id: userData.id,
-      name: userData.name,
-      cpf: userData.cpf,
-    };
 
-    return response.json(userWithoutPassword);
+    return response.json(userData);
 });
 
 export default usersRouter;
