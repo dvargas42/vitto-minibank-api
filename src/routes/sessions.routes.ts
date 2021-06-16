5b5f8596c25c9a31f56d1567e89a8b37aafd7538
dvargas42 <dvargas.eng@gmail.com>
2021-06-16 19:22:40 -0300

    fix: add fied in response

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -18,6 +18,7 @@ sessionsRouter.post('/', async (request, response) => {
     id: user.id,
     name: user.name,
     cpf: user.cpf,
+    balance: user.balance,
     created_at: user.created_at,
     updated_at: user.updated_at,
   };
