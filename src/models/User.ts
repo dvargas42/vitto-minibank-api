81333bcdf43f9699c0063d623cc1f80d4171952b
dvargas42 <dvargas.eng@gmail.com>
2021-06-27 19:51:05 -0300

    feat: implement exclude from class transformer

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity('users')
 class User {
@@ -14,12 +15,14 @@ class User {
   @Column()
   name: string;
 
+  @Exclude()
   @Column()
   cpf: string;
 
   @Column()
   balance: number;
   
+  @Exclude()
   @Column()
   password: string;
 
@@ -30,4 +33,4 @@ class User {
   updated_at: Date;
 }
 
-export default User;
+export { User }
