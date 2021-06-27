d262586696877844298c031ba325ebfe9cca6061
dvargas42 <dvargas.eng@gmail.com>
2021-06-27 19:56:19 -0300

    refactor: implements list transactions service

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-
-import TransactionsRepository from '../repositories/TransactionsRepository'
-import CreateTransactionService from '../services/CreateTransactionService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import CreateTransactionService from '../services/CreateTransactionService'
+import ListTransactionsService from '../services/ListTransactionsService';
 
 const transactionsRouter = Router();
 
@@ -13,22 +12,22 @@ transactionsRouter.use(ensureAuthenticated)
 transactionsRouter.get('/', async (request, response) => {
   const { id } = request.user
 
-  const transactionsRepository = getCustomRepository(TransactionsRepository)
+  const listTransaction = new ListTransactionsService()
 
-  const transactions = await transactionsRepository.find({
-    where: { user_id: id}
-  })
+  const transactions = await listTransaction.execute({id}) 
 
   return response.json(transactions)
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  const { user_id, type, value, cpf } = request.body;
+  const { type, value, cpf } = request.body;
+
+  const { id } = request.user
 
   const createTransaction = new CreateTransactionService();
 
   const transaction = await createTransaction.execute({
-    user_id,
+    user_id: id,
     type,
     value,
     cpf
